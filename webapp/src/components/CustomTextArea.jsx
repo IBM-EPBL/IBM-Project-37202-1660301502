6508967dca9a25a78f576ada8fb619ffa6a83147
Inder pal Singh <as7668105@gmail.com>
2022-11-15 11:03:21 +0530

    Upload missing files

--- webapp/src/components/CustomTextArea.jsx
+++
@@ -1,8 +0,0 @@
-const CustomTextArea = ({ placeholder, hintText }) => {
-    return <div className="input-box">
-
-        <label htmlFor="sd">{placeholder}</label>
-        <textarea placeholder={placeholder} id="sd" multiple rows={8} cols={10} className="text-area" />
-    </div>;
-};
-export default CustomTextArea;
--- webapp/src/provider/store.jsx
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit"
-import customerReducer from "../provider/slices/customerSlice"
-export default configureStore({
-    reducer: {
-        customer: customerReducer,
-
-    }
-})
